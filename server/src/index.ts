e91fdc02ebb7084ea67e49bf6f3fc4a07e0fd189
Nnaji Chimuanya <nnajichimuanyua50@gmail.com>
2022-09-30 04:10:17 -0700

    updated the origin due to cors policy

--- server/src/index.ts
+++ server/src/index.ts
@@ -14,7 +14,7 @@ app.use(express.json());
 app.use(cookieParser());
 app.use(
   cors({
-    origin: "http://tethera.vercel.app",
+    origin: "https://tethera.vercel.app",
     methods: "GET, POST, PUT, DELETE, OPTIONS",
     credentials: true,
   })
